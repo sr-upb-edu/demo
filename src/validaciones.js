4f60f22e24a9c5e41f9482c66ff5ed3bc9918935
sr-upb-edu <sr.upb.edu@gmail.com>
2021-09-21 14:04:58 -0500

    Sprint 2 - archivos base

---
+++ src/validaciones.js
@@ -0,0 +1,15 @@
+function validar_nombre_usuario(string) {
+
+}
+
+function validar_edad_usuario(edad) {
+
+}
+
+function validar_contrasena(string) {
+
+}
+
+module.exports.validar_nombre_usuario = validar_nombre_usuario;
+module.exports.validar_edad_usuario = validar_edad_usuario;
+module.exports.validar_contrasena = validar_contrasena;
