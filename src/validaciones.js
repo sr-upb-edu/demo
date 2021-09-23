a9738ddc4c66a8c955fd2e7384a87fb786a730a6
aiturus <86090771+aiturus@users.noreply.github.com>
2021-09-23 18:12:46 -0500

    Update validaciones.js

--- src/validaciones.js
+++ src/validaciones.js
@@ -1,5 +1,24 @@
 function validar_nombre_usuario(string) {
 
+    var validacion = false;
+    var caracteresPermitidos= /[A-Z\s]+/i;
+    var espacioMayuscula=/^[A-Z]\D+\s?[A-Z]?/;
+    var espaciosNoPermitidos= /\s+$/;
+
+    var string = document.getElementById("dato_nombre_usuario").value;
+
+    if(!caracteresPermitidos.test(string)){
+        validacion;
+    }else if (!espacioMayuscula.test(string)) {
+        validacion;
+    }else if (espaciosNoPermitidos.test(string)) {
+        validacion;
+    }else{
+        validacion = true;
+    }
+    //alert(validacion);
+    return(validacion);
+  
 }
 
 function validar_edad_usuario(edad) {
